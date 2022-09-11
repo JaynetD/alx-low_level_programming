cc16530ba536ac3ba484c290e729f0d8499f5fb5
JaynetD <jaynetdickson@gmail.com>
2022-09-11 14:28:14 -0700

    Hello  puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - printing
+ * main - printing in the put function
+ *
+ * Description: Printing "Programming is like building a multilingual puzzle
  * Return: Always 0 (Successful)
  */
 int main(void)
