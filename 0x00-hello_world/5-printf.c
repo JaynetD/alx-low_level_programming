bde1028be6d057cd47b2a24cad60df82d1f594fb
JaynetD <jaynetdickson@gmail.com>
2022-09-11 15:29:26 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
 * main - print a text
 *
 * Description: using the main function
-* write a program that prints "with proper grammar, but the outcome is a piece of art
-* return: 0
+* this program "Programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
 {
