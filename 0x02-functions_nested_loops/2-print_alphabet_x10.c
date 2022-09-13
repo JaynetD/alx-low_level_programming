f886b0070c2fbb89960c0c0e081de957fe9aabfa
JaynetD <jaynetdickson@gmail.com>
2022-09-13 16:26:13 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_ x10(void)
+{
+	char c, i;
+
+	for (I = 0; i <= 9; 1++)
+	{
+		for (c = ‘a’; c <= ‘z’; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
