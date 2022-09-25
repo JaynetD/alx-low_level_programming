90798f5e5a43a7d7ff0b2624d6d2d7a51edbf209
JaynetD <jaynetdickson@gmail.com>
2022-09-25 16:15:44 -0700

    Do not  hate the hacker but the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+/**
+ * main - generates keygen.
+ * Return: 0 Always.
+ */
+
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
