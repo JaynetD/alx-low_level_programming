b2708e33e366207551a769c5bd7f481d9669699f
JaynetD <jaynetdickson@gmail.com>
2022-10-06 14:05:43 -0700

    Realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,55 +1,57 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * _realloc - reallocates a memory block.
- * @ptr: pointer to the memory previously allocated.
- * @old_size: size, in bytes, of the allocated space of ptr.
- * @new_size: new size, in bytes, of the new memory block.
- *
- * Return: ptr.
- * if new_size == old_size, returns ptr without changes.
- * if malloc fails, returns NULL.
+ * _memcpy - copy memory data from src to dest
+ * @dest: memory destination
+ * @src: memory source
+ * @n: size of new memory
  */
+void _memcpy(void *dest, void *src, size_t n)
+{
+	size_t i;
+	char *csrc = (char *)src;
+	char *cdest = (char *)dest;
+
+	for (i = 0; i < n; i++)
+		cdest[i] = csrc[i];
+}
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: array length
+ * @old_size: size of old memory
+ * @new_size: size of new memory
+ * Return: pointer to new memory
+ */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int i, j;
-	char *newPtr;
 
-	if (old_size == new_size)
-		return (ptr);
-	if (ptr == NULL)
-	{
-		ptr = malloc(new_size);
-		if (ptr == NULL)
-			return (NULL);
-		return (ptr);
-	}
+	void *newPtr;
 
-	if (new_size == 0 && ptr != NULL)
+	if (new_size == 0)
 	{
-		free(ptr);
+		if (ptr != NULL)
+			free(ptr);
 		return (NULL);
 	}
-	newPtr = malloc(new_size * sizeof(char))
-		if (newPtr == NULL)
-			return (NULL);
-	if (new_size > old_size)
+	else if (!ptr)
 	{
-		for (i = 0; i < old_size; i++)
-		{
-			newPtr[i] = ((char *)ptr)[i];
-		}
-		free(ptr);
-		return (newPtr);
+		return (malloc(new_size));
+	}
+	else if (new_size <= old_size)
+	{
+		return (ptr);
 	}
 	else
 	{
-		for (j = 0; j < new_size; j++)
+		newPtr = malloc(new_size);
+		if (newPtr)
 		{
-			newPtr[j] = ((char *)ptr)[j];
+			_memcpy(newPtr, ptr, old_size);
+			free(ptr);
 		}
-		free(ptr);
 		return (newPtr);
 	}
+
+	return (0);
 }
