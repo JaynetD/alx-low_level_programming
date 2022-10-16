e895bfdde37203c9a8d9d362cf60018ef1e3d821
JaynetD <jaynetdickson@gmail.com>
2022-10-16 14:54:55 -0700

    Beauty is variable

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,14 +14,10 @@ int sum_them_all(const unsigned int n, ...)
 	va_list list;
 	unsigned int i, sum = 0;
 
-	if (n == 0)
-		return (0);
-
 	va_start(list, n);
-
-	for (i = 0; i < n; i++)
-		sum += va_arg(list, int);
-
+	if (n != 0)
+		for (i = 0; i < n; sum += va_arg(list, unsigned int), i++)
+			;
 	va_end(list);
 
 	return (sum);
